829d0eac6898acc7c47e2cc4c9da7570cb12de66
alexrybalka <alex.rybalka.2016@gmail.com>
2018-07-16 02:36:06 +0300

    Fixing datetime output and __str__(Vehiclepart)

--- car_showroom/models.py
+++ car_showroom/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class VehiclePart(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField(default=0)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(auto_now=False, auto_now_add=False)
 
     def __str__(self):
-        return "{}. published {}\n{}, price - {},".format(
-            self.id, self.pub_date,
-            self.name, self.price)
+        return "{}: {}; price - {} USD. Published at: {}".format(
+            self.id, self.name,
+            self.price, str(self.pub_date)[:-9])
