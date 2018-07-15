e62b8a0f15ed46d9ca5b6b8b3ce55e05d9e5c839
alexrybalka <alex.rybalka.2016@gmail.com>
2018-07-16 02:12:28 +0300

    Start fixing admin page, adding the CarShowroomConfig to apps

--- autostore/settings.py
+++ autostore/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'car_showroom.apps.CarShowroomConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
