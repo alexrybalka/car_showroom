cd43a0cb7595be1b4f57f494ed357475ac5978cc
alexrybalka <alex.rybalka.2016@gmail.com>
2018-07-17 08:49:48 +0300

    Modifying URLs

--- car_showroom/urls.py
+++ car_showroom/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:vehiclepart_id>/vehiclepart/', views.vehiclepart, name='vehiclepart'),
-    path('<int:section_id>/section/', views.section, name='section'),
+    path('vehiclepart/<int:vehiclepart_id>', views.vehiclepart, name='vehiclepart'),
+    path('section/<int:section_id>/', views.section, name='section'),
 ]
